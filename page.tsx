2ea115ddf6d59346c7dae6e65da3dc1dc1a05d02
AnooshaNadeem12345 <162363213+AnooshaNadeem12345@users.noreply.github.com>
2024-09-20 17:05:51 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import About from './components/About';
+import Header from './components/Header';
+export default function Home() {
+  return (
+    <div>
+    <h1>This is homepage</h1>
+    <Header/>
+    <About/>
+    </div>
+  );
+}
+    
